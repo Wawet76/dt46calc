9d6ef2e43dd33861205cf0757e3da0521b16e735
Thomas Walraet <thomas@walraet.com>
2016-05-20 23:57:49 +0200

    copie et utilisation du tableau

--- test/test.dt46calc.js
+++ test/test.dt46calc.js
@@ -1,11 +1,11 @@
 var assert = chai.assert;
 
 describe('dt46calc', function() {
-  it('should return values from dt46 table', function() {
-      assert.strictEqual(dt46calc(60, 0), 175);
-      assert.strictEqual(dt46calc(60, 2000), 89);
-      assert.strictEqual(dt46calc(110, 100), 276);
-      assert.strictEqual(dt46calc(110, 1600), 138);
-      assert.strictEqual(dt46calc(84, 603), 162);
-  });
+    it('should return values from dt46 table', function() {
+        assert.strictEqual(dt46calc(60, 0), 175);
+        assert.strictEqual(dt46calc(60, 2000), 89);
+        assert.strictEqual(dt46calc(110, 100), 276);
+        assert.strictEqual(dt46calc(110, 1600), 138);
+        assert.strictEqual(dt46calc(84, 603), 162);
+    });
 });
